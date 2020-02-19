185b6a7681915e6c0cf7ddcdb246b6a3a7136f2b
Victor Tu <46986265+dashprotocol@users.noreply.github.com>
2020-02-18 16:42:00 -0800

    Refactor helper function to create 3 million records

--- database/seed/postgres/users.js
+++ database/seed/postgres/users.js
@@ -1,35 +1,38 @@
 const faker = require('faker');
 const fs = require('fs');
 
-const eliteYears = [
-  2020, null, null, null, null, null, null, null, null, 2019,
-  null, null, null, null, null, 2018, null, null, null, null, null,
-];
+const eliteYears = [2018, 2019, 2020];
 
-const randomEliteYear = () => eliteYears[Math.floor(Math.random() * Math.floor(20))];
+const randomEliteYear = () => {
+  const randomChance = Math.random();
+  if (randomChance < 0.02) {
+    return eliteYears[Math.floor(Math.random() * Math.floor(3))];
+  }
+  return '';
+};
 
 const writeUsers = fs.createWriteStream('usersPostGres.csv');
 writeUsers.write('id,first_name,last_name,profile_pic,profile_url,city,state,creation_date,friends_count,photos_count,elite_year\n', 'utf8');
 
-const writeFourMillion = (writer, encoding, callback) => {
-  let i = 1000;
+const writeThreeMillion = (writer, encoding, callback) => {
+  let i = 3000000;
   let id = 0;
   const write = () => {
     let ok = true;
     do {
       i -= 1;
       id += 1;
-      const first_name = faker.name.firstName();
-      const last_name = faker.name.lastName();
-      const profile_pic = faker.image.avatar();
-      const profile_url = faker.internet.url();
+      const firstName = faker.name.firstName();
+      const lastName = faker.name.lastName();
+      const profilePic = faker.image.avatar();
+      const profileUrl = faker.internet.url();
       const city = faker.address.city();
       const state = faker.address.state();
-      const creation_date = faker.date.past();
-      const friends_count = faker.random.number();
-      const photos_count = faker.random.number();
-      const elite_year = randomEliteYear();
-      const data = `${id},${first_name},${last_name},${profile_pic},${profile_url},${city},${state},${creation_date},${friends_count},${photos_count},${elite_year}\n`;
+      const creationDate = faker.date.past();
+      const friendsCount = faker.random.number();
+      const photosCount = faker.random.number();
+      const eliteYear = randomEliteYear();
+      const data = `${id},${firstName},${lastName},${profilePic},${profileUrl},${city},${state},${creationDate},${friendsCount},${photosCount},${eliteYear}\n`;
       if (i === 0) {
         writer.write(data, encoding, callback);
       } else {
@@ -43,6 +46,6 @@ const writeFourMillion = (writer, encoding, callback) => {
   write();
 };
 
-writeFourMillion(writeUsers, 'utf-8', () => {
+writeThreeMillion(writeUsers, 'utf-8', () => {
   writeUsers.end();
 });
