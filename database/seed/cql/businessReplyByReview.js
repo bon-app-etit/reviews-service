0d9be52df0a7a1495b3b87b5fdef16e3a9f94b05
Victor Tu <46986265+dashprotocol@users.noreply.github.com>
2020-02-18 23:13:05 -0800

    Add script to seed cassandra business reply table

---
+++ database/seed/cql/businessReplyByReview.js
@@ -0,0 +1,40 @@
+const faker = require('faker');
+const fs = require('fs');
+
+const writeBusinessReplies = fs.createWriteStream('Business_RepliesCql.csv');
+writeBusinessReplies.write('business_reply_id,review_id,name,business_position,business_avatar,reply_date,reply_text\n', 'utf8');
+
+const randomReviewId = () => Math.floor(Math.random() * Math.floor(10000000));
+
+const writeOneMillion = (writer, encoding, callback) => {
+  let i = 1000000;
+  let id = 0;
+  const write = () => {
+    let ok = true;
+    do {
+      i -= 1;
+      id += 1;
+      const reviewId = randomReviewId();
+      const name = faker.name.firstName();
+      const businessPosition = faker.name.jobTitle();
+      const businessAvatar = faker.image.avatar();
+      const replyDate = faker.date.past();
+      const replyText = faker.lorem.sentences();
+      const data = `${id},${reviewId},${name},${businessPosition},${businessAvatar}, ${replyDate},${replyText}\n`;
+      if (i === 0) {
+        writer.write(data, encoding, callback);
+      } else {
+        ok = writer.write(data, encoding);
+      }
+    } while (i > 0 && ok);
+    if (i > 0) {
+      writer.once('drain', write);
+    }
+  };
+  write();
+};
+
+writeOneMillion(writeBusinessReplies, 'utf-8', () => {
+  console.log('data generation completed');
+  writeBusinessReplies.end();
+});
