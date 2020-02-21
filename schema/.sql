8b01670077818b0cb5a1edd67ec0905208eaa914
Victor Tu <46986265+dashprotocol@users.noreply.github.com>
2020-02-20 21:20:35 -0800

    Update schema

--- schema/.sql
+++ schema/.sql
@@ -3,9 +3,9 @@ CREATE DATABASE BONAPPETIT;
 -- connect to database
 \c BONAPPETIT
 
-CREATE SCHEMA IF NOT EXISTS restaurant_reviews;
-    CREATE TABLE restaurant_reviews.users(
-      id SERIAL PRIMARY KEY,
+
+    CREATE TABLE users(
+      user_id SERIAL PRIMARY KEY,
       first_name VARCHAR(80) NOT NULL,
       last_name text NOT NULL,
       profile_pic VARCHAR NOT NULL,
@@ -17,8 +17,8 @@ CREATE SCHEMA IF NOT EXISTS restaurant_reviews;
       photos_count INTEGER NOT NULL,
       elite_year INTEGER
     );
-    CREATE TABLE restaurant_reviews.restaurants(
-      id SERIAL PRIMARY KEY,
+    CREATE TABLE restaurants(
+      restaurant_id SERIAL PRIMARY KEY,
       name VARCHAR NOT NULL,
       address_1 VARCHAR NOT NULL,
       address_2 VARCHAR NOT NULL,
@@ -30,41 +30,41 @@ CREATE SCHEMA IF NOT EXISTS restaurant_reviews;
       phone_number VARCHAR NOT NULL,
       website VARCHAR
     );
-    CREATE TABLE restaurant_reviews.reviews(
-      id SERIAL PRIMARY KEY,
-      user_id INTEGER REFERENCES users(id) NOT NULL,
-      restaurant_id INTEGER REFERENCES restaurants(id) NOT NULL,
+    CREATE TABLE reviews(
+      review_id SERIAL PRIMARY KEY,
+      user_id INTEGER REFERENCES users(user_id) NOT NULL,
+      restaurant_id INTEGER REFERENCES restaurants(restaurant_id) NOT NULL,
       rating INTEGER NOT NULL,
       review_date VARCHAR NOT NULL,
       review_text VARCHAR NOT NULL,
-      previous_review INTEGER REFERENCES reviews(id)
+      previous_review INTEGER REFERENCES reviews(review_id)
     );
-    CREATE TABLE restaurant_reviews.photos(
-      id SERIAL PRIMARY KEY,
-      user_id INTEGER REFERENCES users(id) NOT NULL,
-      review_id INTEGER REFERENCES reviews(id) NOT NULL,
+    CREATE TABLE photos(
+      photo_id SERIAL PRIMARY KEY,
+      user_id INTEGER REFERENCES users(user_id) NOT NULL,
+      review_id INTEGER REFERENCES reviews(review_id) NOT NULL,
       photo_url VARCHAR NOT NULL,
       photo_text VARCHAR
     );
-    CREATE TABLE restaurant_reviews.business_replies(
-      id SERIAL PRIMARY KEY,
-      review_id INTEGER REFERENCES reviews(id) NOT NULL,
+    CREATE TABLE business_replies(
+      business_reply_id SERIAL PRIMARY KEY,
+      review_id INTEGER REFERENCES reviews(review_id) NOT NULL,
       name VARCHAR NOT NULL,
       business_position TEXT NOT NULL,
       business_avatar TEXT NOT NULL,
       reply_date VARCHAR NOT NULL,
       reply_text VARCHAR NOT NULL
     );
-    CREATE TABLE restaurant_reviews.reviews_users_votes(
-      review_id INTEGER REFERENCES reviews(id) NOT NULL,
-      user_id INTEGER REFERENCES users(id) NOT NULL,
+    CREATE TABLE reviews_users_votes(
+      review_id INTEGER REFERENCES reviews(review_id) NOT NULL,
+      user_id INTEGER REFERENCES users(user_id) NOT NULL,
       voted_cool BOOLEAN NOT NULL,
       voted_funny BOOLEAN NOT NULL,
       voted_useful BOOLEAN NOT NULL
     );
-    CREATE TABLE restaurant_reviews.photos_users_votes(
-      photo_id INTEGER REFERENCES photos(id),
-      user_id INTEGER REFERENCES users(id),
+    CREATE TABLE photos_users_votes(
+      photo_id INTEGER REFERENCES photos(photo_id),
+      user_id INTEGER REFERENCES users(user_id),
       voted_helpful BOOLEAN NOT NULL,
       voted_unhelpful BOOLEAN NOT NULL
     );
