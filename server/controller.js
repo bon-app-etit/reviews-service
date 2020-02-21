cff82790a598c5bd02a31aee6bf8493c01472a73
Victor Tu <46986265+dashprotocol@users.noreply.github.com>
2020-02-21 15:58:08 -0800

    Add ability to get all reviews for a given restaurant

--- server/controller.js
+++ server/controller.js
@@ -1,90 +1,90 @@
+const db = require('../database/index.js');
 
 module.exports = {
   getReviews: (req, res) => {
     const { id } = req.params;
     db.readAllReviews(id, (err, reviews) => {
       if (err) {
-        console.log('controller error: getReviews');
         res.status(500).end();
       } else {
         res.send(reviews);
       }
     });
   },
-  postReview: (req, res) => {
-    const { name } = req.parmas;
-    db.createReview(name, req.body, (err, results) => {
-      if (err) {
-        console.log('controller error: postReview');
-        res.status(500);
-      } else {
-        res.status(201);
-      }
-    });
-  },
-  putReview: (req, res) => {
-    const { id } = req.params;
-    db.updateReview(id, req.body, (err, results) => {
-      if (err) {
-        console.log('controller error: putReview');
-        res.status(500);
-      } else {
-        res.status(201);
-      }
-    });
-  },
-  deleteReview: (req, res) => {
-    const { id } = req.params;
-    db.deleteReview(id, (err, results) => {
-      if (err) {
-        console.log('controller error: deleteReview');
-        res.status(500);
-      } else {
-        res.status(201);
-      }
-    });
-  },
-  getUser: (req, res) => {
-    const { id } = req.params;
-    db.readUser(id, (err, user) => {
-      if (err) {
-        console.log('controller error: getUser');
-        res.status(500).end();
-      } else {
-        res.send(user);
-      }
-    });
-  },
-  postUser: (req, res) => {
-    db.createUser(req.body, (err, results) => {
-      if (err) {
-        console.log('controller error: postUser');
-        res.status(500);
-      } else {
-        res.status(201);
-      }
-    });
-  },
-  putUser: (req, res) => {
-    const { id } = req.params;
-    db.updateUser(id, req.body, (err, results) => {
-      if (err) {
-        console.log('controller error: putUser');
-        res.status(500);
-      } else {
-        res.status(201);
-      }
-    });
-  },
-  deleteUser: (req, res) => {
-    const { id } = req.params;
-    db.deleteUser(id, (err, results) => {
-      if (err) {
-        console.log('controller error: deleteUser');
-        res.status(500);
-      } else {
-        res.status(201);
-      }
-    });
-  },
+  // postReview: (req, res) => {
+  //   const { name } = req.parmas;
+  //   db.createReview(name, req.body, (err, results) => {
+  //     if (err) {
+  //       console.log('controller error: postReview');
+  //       res.status(500);
+  //     } else {
+  //       res.status(201);
+  //     }
+  //   });
+  // },
+  // putReview: (req, res) => {
+  //   const { id } = req.params;
+  //   db.updateReview(id, req.body, (err, results) => {
+  //     if (err) {
+  //       console.log('controller error: putReview');
+  //       res.status(500);
+  //     } else {
+  //       res.status(201);
+  //     }
+  //   });
+  // },
+  // deleteReview: (req, res) => {
+  //   const { id } = req.params;
+  //   db.deleteReview(id, (err, results) => {
+  //     if (err) {
+  //       console.log('controller error: deleteReview');
+  //       res.status(500);
+  //     } else {
+  //       res.status(201);
+  //     }
+  //   });
+  // },
+  // getUser: (req, res) => {
+  //   const { id } = req.params;
+  //   db.readUser(id, (err, user) => {
+  //     if (err) {
+  //       console.log('controller error: getUser');
+  //       res.status(500).end();
+  //     } else {
+  //       res.send(user);
+  //     }
+  //   });
+  // },
+  // postUser: (req, res) => {
+  //   db.createUser(req.body, (err, results) => {
+  //     if (err) {
+  //       console.log('controller error: postUser');
+  //       res.status(500);
+  //     } else {
+  //       res.status(201);
+  //     }
+  //   });
+  // },
+  // putUser: (req, res) => {
+  //   const { id } = req.params;
+  //   db.updateUser(id, req.body, (err, results) => {
+  //     if (err) {
+  //       console.log('controller error: putUser');
+  //       res.status(500);
+  //     } else {
+  //       res.status(201);
+  //     }
+  //   });
+  // },
+  // deleteUser: (req, res) => {
+  //   const { id } = req.params;
+  //   db.deleteUser(id, (err, results) => {
+  //     if (err) {
+  //       console.log('controller error: deleteUser');
+  //       res.status(500);
+  //     } else {
+  //       res.status(201);
+  //     }
+  //   });
+  // },
 };
--- server/index.js
+++ server/index.js
@@ -1,135 +1,79 @@
 const express = require('express');
-const path = require('path');
 const morgan = require('morgan');
-const axios = require('axios');
-const controller = './controller.js';
+
+const controller = require('./controller.js');
 
 const app = express();
 const PORT = 3007;
 
+app.use(express.json());
 app.use(morgan('dev'));
 
 // Get all reviews
 app.get('/restaurant/:id/reviews', controller.getReviews);
 
-// Create a review
-app.post('/restaurant/:restaurant_id/user/:user_id/review', controller.postReview);
+// // Create a review
+// app.post('/restaurant/:restaurant_id/user/:user_id/review', controller.postReview);
 
-// Update a review
-app.put('/review/:id', controller.putReview);
+// // Update a review
+// app.put('/review/:id', controller.putReview);
 
-// Delete a review
-app.delete('/review/:id', controller.deleteReview);
+// // Delete a review
+// app.delete('/review/:id', controller.deleteReview);
 
-// Get a user
-app.get('/user/:id', controller.getUser);
+// // Create a user
+// app.post('/user', controller.postUser);
 
-// Create a user
-app.post('/user', controller.postUser);
+// // Update a user
+// app.put('/user/:id', controller.putUser);
 
-// Update a user
-app.put('/user/:id', controller.putUser);
+// // Delete a user
+// app.delete('/user/:id', controller.deleteUser);
 
-// Delete a user
-app.delete('/user/:id', controller.deleteUser);
+// // Add a photo to a review
+// app.post('/review/:id/photo', controller.postReviewPhoto);
 
-// Get all photos for a review
-app.get('/review/:id/photos', controller.getReviewPhotos);
+// // Update a photo for a review
+// app.put('/photo/:id', controller.putReviewPhoto);
 
-// Add a photo to a review
-app.post('/review/:id/photo', controller.postReviewPhoto);
+// // Delete a photo from a review
+// app.delete('/photo/:id', controller.deleteReviewPhoto);
 
-// Update a photo for a review
-app.put('/photo/:id', controller.putReviewPhoto);
+// // Add a business reply for a review
+// app.post('/review/:id/business_reply', controller.postBusinessReply);
 
-// Delete a photo from a review
-app.delete('/photo/:id', controller.deleteReviewPhoto);
+// // Update a business reply for a review
+// app.put('/business_reply/:id', controller.putBusinessReply);
 
-// Get a business reply for a review
-app.get('/review/:id/business_reply', controller.getBusinessReply);
+// // Delete a business reply from a review
+// app.delete('/business_reply/:id', controller.deleteBusinessReply);
 
-// Add a business reply for a review
-app.post('/review/:id/business_reply', controller.postBusinessReply);
+// // Create a restaurant
+// app.post('/restaurant', controller.postRestaurant);
 
-// Update a business reply for a review
-app.put('/business_reply/:id', controller.putBusinessReply);
+// // Update a restaurant
+// app.put('/restaurant/:id', controller.putRestaurant);
 
-// Delete a business reply from a review
-app.delete('/business_reply/:id', controller.deleteBusinessReply);
+// // Delete a restaurant
+// app.delete('/restaurant/:id', controller.deleteRestaurant);
 
-// Get a restaurant
-app.get('/restaurant/:id', controller.getRestaurant);
+// // Add votes from a user for a review
+// app.post('/review/:review_id/user/:user_id/votes', controller.addReviewVotes);
 
-// Create a restaurant
-app.post('/restaurant', controller.postRestaurant);
+// // Update votes from a user for a review
+// app.put('/review/:review_id/user/:user_id/votes', controller.putReviewVotes);
 
-// Update a restaurant
-app.put('/restaurant/:id', controller.putRestaurant);
+// // Delete votes from a user for a review
+// app.delete('/review/:review_id/user/:user_id/votes', controller.deleteReviewVotes);
 
-// Delete a restaurant
-app.delete('/restaurant/:id', controller.deleteRestaurant);
+// // Add vote from a user for a photo
+// app.post('/photo/:photo_id/user/:user_id/vote', controller.addPhotoVotes);
 
-// Get all votes for a review
-app.get('/review/:id/votes', controller.getReviewVotes);
+// // Update vote from a user for a photo
+// app.put('/photo/:photo_id/user/:user_id/vote', controller.putPhotoVotes);
 
-// Add votes from a user for a review
-app.post('/review/:review_id/user/:user_id/votes', controller.addReviewVotes);
-
-// Update votes from a user for a review
-app.put('/review/:review_id/user/:user_id/votes', controller.putReviewVotes);
-
-// Delete votes from a user for a review
-app.delete('/review/:review_id/user/:user_id/votes', controller.deleteReviewVotes);
-
-// Get vote for a photo from a user
-app.get('/photo/:photo_id/user/:user_id/vote', controller.getPhotoVotes);
-
-// Add vote from a user for a photo
-app.post('/photo/:photo_id/user/:user_id/vote', controller.addPhotoVotes);
-
-// Update vote from a user for a photo
-app.put('/photo/:photo_id/user/:user_id/vote', controller.putPhotoVotes);
-
-// Delete votes from a user for a photo
-app.delete('/photo/:photo_id/user/:user_id/votes', controller.deletePhotoVotes);
+// // Delete votes from a user for a photo
+// app.delete('/photo/:photo_id/user/:user_id/votes', controller.deletePhotoVotes);
 
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
-
-
-// Use cases
-  // Reviews
-    // Get all reviews
-    // Create a review
-    // Update a review
-    // Delete a review
-  // Users
-    // Get a user
-    // Create a user
-    // Update a user
-    // Delete a user
-  // Photos
-    // Get all photos for a review
-    // Add a photo to a review
-    // Update a photo to a review
-    // Delete a photo from a review
-  // Business Replies
-    // Get a business reply for a review
-    // Add a business reply to a review
-    // Update a business reply to a review
-    // Delete a business reply from a review
-  // Restaurants
-    // Get a restaurant
-    // Create a restaurant
-    // Update a restaurant
-    // Delete a restaurant
-  // Review Votes
-    // Get votes for a review
-    // Add votes from a user for a review
-    // Update votes from a user for a review
-    // Delete votes from a user for a review
-  // Photo Votes (Only displays if user voted, doesn't display all votes from other users)
-    // Get vote for a photo from a user
-    // Add votes from a user for a photo
-    // Update votes from a user for a photo
-    // Delete votes from a user for a photo
